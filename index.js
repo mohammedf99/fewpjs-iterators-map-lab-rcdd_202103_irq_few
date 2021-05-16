b809282642138ce951ea4add76d9e22512836219
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-16 17:15:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const titleCased = () => {
     let sentence = tutorial.split(" ");
     
     for (let i = 0; i < sentence.length; i++) {
-      sentence[i] = sentence[i][0].toUpperCase + sentence[i].splice(1);
+      sentence[i] = sentence[i].charAt(0).toUpperCase + sentence[i].slice(1);
     }
     
     return sentence.join(" ");
